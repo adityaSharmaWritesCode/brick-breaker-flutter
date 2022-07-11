e5ec4408b0a11cb0664f6d719891f8af65b2355b
unknown <shaditya1604@gmail.com>
2022-07-11 21:27:14 +0530

    Added bricks and varying speeds

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:collection';
+import 'dart:html';
 
 import 'package:brick_breaker/bricks.dart';
 import 'package:brick_breaker/player.dart';
@@ -36,18 +38,46 @@ class _HomeScreenState extends State<HomeScreen> {
       0.2; //(Three working values : 0.1, 0.2, 0.4, the larger, the faster it moves)
 
   //Brick variables :-
-  double wallGap = 0.2;
-  int numOfBricksPerRow = 4;
-  static double firstBrickX = -0.5;
+  static double wallGap = 0.5 *
+      (2 - numOfBricksPerRow * brickWidth - (numOfBricksPerRow - 1) * brickGap);
+  static int numOfBricksPerRow = 4;
+  static double firstBrickX = -1 + wallGap;
   static double firstBrickY = -0.9;
   static double brickWidth = 0.4;
   static double brickHeight = 0.05;
-  double brickGap = 0.2;
-  bool bb = false;
+  static double brickGap = 0.05;
+
+  List MyBricks = [
+    [firstBrickX, firstBrickY, false],
+    [firstBrickX + 1 * (brickWidth + brickGap), firstBrickY, false],
+    [firstBrickX + 2 * (brickWidth + brickGap), firstBrickY, false],
+    [firstBrickX + 3 * (brickWidth + brickGap), firstBrickY, false],
+    [
+      firstBrickX + 0 * (brickWidth + brickGap),
+      firstBrickY + 1 * (brickHeight + brickGap),
+      false
+    ],
+    [
+      firstBrickX + 1 * (brickWidth + brickGap),
+      firstBrickY + 1 * (brickHeight + brickGap),
+      false
+    ],
+    [
+      firstBrickX + 2 * (brickWidth + brickGap),
+      firstBrickY + 1 * (brickHeight + brickGap),
+      false
+    ],
+    [
+      firstBrickX + 3 * (brickWidth + brickGap),
+      firstBrickY + 1 * (brickHeight + brickGap),
+      false
+    ],
+  ];
 
   //Game settings :-
   bool hasGameStarted = false;
   bool hasGameEnded = false;
+  int brokenBrickCounter = 0;
 
   //ALL FUNCTIONS :-
   void startGame() {
@@ -67,7 +97,7 @@ class _HomeScreenState extends State<HomeScreen> {
         //  1. stop the timer,
         //  2. update the game state booleans
 
-        if (isPlayerDead()) {
+        if (isPlayerDead() || brokenBrickCounter == MyBricks.length) {
           timer.cancel();
           setState(() {
             hasGameEnded = true;
@@ -77,23 +107,88 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  // List<Widget> generateBricks() {
+  //   List<Widget> sol = [];
+  //   for (int i = 0; i < MyBricks.length; i++) {
+  //     sol.add(
+  //       MyBrick(
+  //         brickX: MyBricks[i][0],
+  //         brickY: MyBricks[i][1],
+  //         brickHeight: brickHeight,
+  //         brickWidth: brickWidth,
+  //         brickBroken: MyBricks[i][2],
+  //       ),
+  //     );
+  //   }
+
+  //   return sol;
+  // }
+
   void checkForBrokenBricks() {
-    if (ballX >= firstBrickX &&
-            ballX <= firstBrickX + brickWidth &&
-            ballY <= firstBrickY + brickHeight &&
-            bb == false
-        //&& ballY >= firstBrickY) {
-        ) {
-      setState(() {
-        bb = true;
-        //update ball's direction
-        ballYdir = direction.DOWN;
-      });
+    for (int i = 0; i < MyBricks.length; i++) {
+      if (ballX >= MyBricks[i][0] &&
+              ballX <= MyBricks[i][0] + brickWidth &&
+              ballY <= MyBricks[i][1] + brickHeight &&
+              MyBricks[i][2] == false
+          // && ballY >= MyBricks[i][1]) {
+          ) {
+        setState(() {
+          MyBricks[i][2] = true;
+          brokenBrickCounter++;
+          //Update ball's direction
+          //Now to do this, we must determine which side of the brick has been hit
+          // as that influences the direction in which the ball has to be reflected
+
+          //To do this, we can compute the distance of the ball from each side of the brick
+          //The shortest distance will correspond to the side of the brick that has been hit
+          double leftSideDist = (MyBricks[i][0] - ballX).abs();
+          double rightSideDist = (MyBricks[i][0] + brickWidth - ballX).abs();
+          double topSideDist = (MyBricks[i][1] - ballY).abs();
+          double bottomSideDist = (MyBricks[i][1] + brickHeight - ballY).abs();
+
+          String min = findMinDist(
+              leftSideDist, rightSideDist, topSideDist, bottomSideDist);
+          print(min);
+          switch (min) {
+            case 'l':
+              ballXdir = direction.LEFT;
+              break;
+            case 'r':
+              ballXdir = direction.RIGHT;
+              break;
+            case 't':
+              ballYdir = direction.UP;
+              break;
+            case 'b':
+              ballYdir = direction.DOWN;
+              break;
+          }
+        });
+      }
     }
   }
 
+  String findMinDist(double l, double r, double t, double b) {
+    double mini = l;
+    if (mini > r) mini = r;
+    if (mini > t) mini = t;
+    if (mini > b) mini = b;
+
+    if ((mini - l).abs() < 0.01) {
+      return 'l';
+    } else if ((mini - r).abs() < 0.01) {
+      return 'r';
+    } else if ((mini - t).abs() < 0.01) {
+      return 't';
+    } else if ((mini - l).abs() < 0.01) {
+      return 'b';
+    }
+
+    return '';
+  }
+
   bool isPlayerDead() {
-    return (ballY > 0.96);
+    return (ballY > 0.94);
   }
 
   void updateBallDirection() {
@@ -134,9 +229,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
       //Horizontal Movement :
       if (ballXdir == direction.RIGHT) {
-        ballX += ballSpeed;
+        ballX += 2 * ballSpeed;
       } else if (ballXdir == direction.LEFT) {
-        ballX -= ballSpeed;
+        ballX -= 2 * ballSpeed;
       }
     });
   }
@@ -155,6 +250,22 @@ class _HomeScreenState extends State<HomeScreen> {
         playerX += playerSpeed;
       });
     }
+  }
+
+  void resetGame() {
+    setState(() {
+      hasGameEnded = false;
+      hasGameStarted = false;
+      brokenBrickCounter = 0;
+      ballX = 0.0;
+      ballY = 0.0;
+      direction ballXdir = direction.NULL;
+      direction ballYdir = direction.DOWN;
+      playerX = -0.2;
+      for (int i = 0; i < MyBricks.length; i++) {
+        MyBricks[i][2] = false;
+      }
+    });
   }
 
   @override
@@ -193,16 +304,24 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
 
-                  //2. Game Over :-
+                  //2. Game Over Screen :-
                   Visibility(
                       visible: hasGameEnded,
-                      child: const Align(
-                        alignment: Alignment(0.0, 0.0),
-                        child: Text(
-                          'G A M E  O V E R !',
-                          style: TextStyle(
-                            color: Colors.teal,
-                          ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'G A M E  O V E R !',
+                              style: TextStyle(
+                                color: Colors.teal,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: resetGame,
+                              child: Text('Retry?'),
+                            ),
+                          ],
                         ),
                       )),
 
@@ -223,12 +342,65 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
 
                   //BRICKS
+
+                  //Row 1 :-
                   MyBrick(
-                    brickX: firstBrickX,
-                    brickY: firstBrickY,
+                    brickX: MyBricks[0][0],
+                    brickY: MyBricks[0][1],
                     brickHeight: brickHeight,
                     brickWidth: brickWidth,
-                    brickBroken: bb,
+                    brickBroken: MyBricks[0][2],
+                  ),
+                  MyBrick(
+                    brickX: MyBricks[1][0],
+                    brickY: MyBricks[1][1],
+                    brickHeight: brickHeight,
+                    brickWidth: brickWidth,
+                    brickBroken: MyBricks[1][2],
+                  ),
+                  MyBrick(
+                    brickX: MyBricks[2][0],
+                    brickY: MyBricks[2][1],
+                    brickHeight: brickHeight,
+                    brickWidth: brickWidth,
+                    brickBroken: MyBricks[2][2],
+                  ),
+                  MyBrick(
+                    brickX: MyBricks[3][0],
+                    brickY: MyBricks[3][1],
+                    brickHeight: brickHeight,
+                    brickWidth: brickWidth,
+                    brickBroken: MyBricks[3][2],
+                  ),
+
+                  //Row 2 :-
+                  MyBrick(
+                    brickX: MyBricks[4][0],
+                    brickY: MyBricks[4][1],
+                    brickHeight: brickHeight,
+                    brickWidth: brickWidth,
+                    brickBroken: MyBricks[4][2],
+                  ),
+                  MyBrick(
+                    brickX: MyBricks[5][0],
+                    brickY: MyBricks[5][1],
+                    brickHeight: brickHeight,
+                    brickWidth: brickWidth,
+                    brickBroken: MyBricks[5][2],
+                  ),
+                  MyBrick(
+                    brickX: MyBricks[6][0],
+                    brickY: MyBricks[6][1],
+                    brickHeight: brickHeight,
+                    brickWidth: brickWidth,
+                    brickBroken: MyBricks[6][2],
+                  ),
+                  MyBrick(
+                    brickX: MyBricks[7][0],
+                    brickY: MyBricks[7][1],
+                    brickHeight: brickHeight,
+                    brickWidth: brickWidth,
+                    brickBroken: MyBricks[7][2],
                   ),
                 ],
               ),
